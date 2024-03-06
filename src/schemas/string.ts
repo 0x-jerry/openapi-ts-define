abc9d8fc6fc51d5983a0ff4da50fa0d2ca0a4cbc
Jerry <x.jerry.wang@gmail.com>
2024-03-06 18:00:42 +0800

    fix: handle no substitution template literal

--- src/schemas/string.ts
+++ src/schemas/string.ts
@@ -1,12 +1,12 @@
 import type { JSONSchema7 } from 'json-schema'
 import tsm, { Node } from 'ts-morph'
 
-export function toStringSchema(node: tsm.StringLiteral | tsm.Expression): JSONSchema7 {
+export function toStringSchema(node: tsm.StringLiteral | tsm.NoSubstitutionTemplateLiteral | tsm.Expression): JSONSchema7 {
   const schema: JSONSchema7 = {
     type: 'string',
   }
 
-  if (Node.isStringLiteral(node)) {
+  if (Node.isStringLiteral(node) || Node.isNoSubstitutionTemplateLiteral(node)) {
     schema.const = node.getLiteralValue()
   }
 
