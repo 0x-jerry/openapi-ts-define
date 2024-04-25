2f552baf1486eba0a1c7f90c4c10cfaf9cce01ef
0x-jerry <x.jerry.wang@gmail.com>
2024-04-25 09:37:38 +0800

    perf: support extract root route

---
+++ src/extractor/nitro.test.ts
@@ -0,0 +1,78 @@
+import { _convertToUrlPath } from "./nitro"
+
+describe('netro extractor', () => {
+  it('convert to url path', () => {
+    const tests = [
+      {
+        path: 'api/index.ts',
+        expected: {
+          path: '/api',
+          method: 'get',
+          params: []
+        }
+      },
+      {
+        path: 'api/hello.ts',
+        expected: {
+          path: '/api/hello',
+          method: 'get',
+          params: []
+        }
+      },
+      {
+        path: 'api/hello.get.ts',
+        expected: {
+          path: '/api/hello',
+          method: 'get',
+          params: []
+        }
+      },
+      {
+        path: 'api/hello.post.ts',
+        expected: {
+          path: '/api/hello',
+          method: 'post',
+          params: []
+        }
+      },
+      {
+        path: 'api/user/[id].get.ts',
+        expected: {
+          path: '/api/user/{id}',
+          method: 'get',
+          params: [{
+            name: 'id'
+          }]
+        }
+      },
+      {
+        path: 'api/user/[id].post.ts',
+        expected: {
+          path: '/api/user/{id}',
+          method: 'post',
+          params: [{
+            name: 'id'
+          }]
+        }
+      },
+      {
+        path: 'api/user/[id]/[name].put.ts',
+        expected: {
+          path: '/api/user/{id}/{name}',
+          method: 'put',
+          params: [{
+            name: 'id'
+          }, {
+            name: 'name'
+          }]
+        }
+      },
+    ]
+
+    for (const t of tests) {
+      const resp = _convertToUrlPath(t.path)
+      expect(resp).eqls(t.expected)
+    }
+  })
+
+})
--- src/extractor/nitro.ts
+++ src/extractor/nitro.ts
@@ -9,17 +9,19 @@ const methodRE = /\.(?<method>get|post|put|delete|patch)$/i
  *
  * Supported path format:
  *
- * - api/hello.ts => GET /api/hello
- * - api/hello.get.ts => GET /api/hello
- * - api/hello.post.ts => POST /api/hello
- * - api/user/[id].get.ts => GET /api/user/:id
- * - api/user/[id].post.ts => POST /api/user/:id
- * - api/user/[id]/[name].put.ts => PUT /api/user/:id/:name
+ * - api/index.ts => GET /api => /api
+ * - api/hello.ts => GET /api/hello => /api/hello
+ * - api/hello.get.ts => GET /api/hello => /api/hello
+ * - api/hello.post.ts => POST /api/hello => /api/hello
+ * - api/user/[id].get.ts => GET /api/user/:id => /api/user/{id}
+ * - api/user/[id].post.ts => POST /api/user/:id => /api/user/{id}
+ * - api/user/[id]/[name].put.ts => PUT /api/user/:id/:name => /api/user/{id}/{name}
  *
  * @param relativeFilePath
+ * @private
  * @returns
  */
-function convertToUrlPath(relativeFilePath: string) {
+export function _convertToUrlPath(relativeFilePath: string) {
   let method = 'get'
 
   const params: RouteRequestParam[] = []
@@ -51,6 +53,10 @@ function convertToUrlPath(relativeFilePath: string) {
     }
   })
 
+  if (urlSegments.at(-1) === 'index') {
+    urlSegments.splice(urlSegments.length - 1, 1)
+  }
+
   const urlPath = '/' + urlSegments.join('/')
 
   return {
@@ -77,7 +83,7 @@ const nitroExtractor: RouteInfoExtractor = (source, ctx) => {
     return
   }
 
-  const routeInfo = convertToUrlPath(ctx.path)
+  const routeInfo = _convertToUrlPath(ctx.path)
 
   const routeConfig: RouteInfo = {
     path: routeInfo.path,
