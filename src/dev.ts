6d2f0420e348e1357095fa45c83b20c5acd4b234
Jerry <x.jerry.wang@gmail.com>
2025-01-13 07:31:17 +0000

    chore: update dev script

--- src/dev.ts
+++ src/dev.ts
@@ -1,28 +1,29 @@
-import path from 'path'
-import { Project, Node } from 'ts-morph'
-import type { ToSchemaContext } from './schemas/types'
-import { RefsManager } from './schemas/utils'
-import { toSchema } from './schemas/schema'
+import path from "path"
+import { OpenAPIGenerator } from "."
+import { RoutesParser } from "./RoutesParser"
 
-const project = new Project({
-  tsConfigFilePath: 'tsconfig.json',
+const generator = OpenAPIGenerator({
+  openAPI: {
+    info: {
+      version: '1.0.0',
+      title: 'test spec',
+    },
+  },
 })
 
-const cwd = path.join(import.meta.dirname, 'schemas/examples')
+const parser = new RoutesParser({
+  tsconfig: 'tsconfig.json',
+  refsManager: generator.refsManager!,
+})
 
-const ctx: ToSchemaContext = {
-  cwd,
-  project,
-  refs: new RefsManager(),
-  nodeStack: [],
+parser.parse({
+  routesRoot: path.resolve(`tests/features/infer-type`),
+  matchFiles: ['**/*.ts', '!**/_*.ts'],
+})
+
+const output = {
+  refs: parser.schemaContext.refs.data,
+  routes: parser.routes,
 }
 
-const absFile = path.join(cwd, 'generic.ts')
-
-const source = project.getSourceFileOrThrow(absFile)
-
-const exportNodes = source.getExportSymbols().map((item) => item.getDeclarations().at(0)!)
-
-const schemas = exportNodes.map((n) => toSchema(n, ctx))
-
-console.log(JSON.stringify(schemas, null, 2))
+console.log(output)
